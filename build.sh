635702c77dec2bdbdce102a13965f6537476bddd
drzoidberg <a@b.de>
2015-05-01 14:11:20 +0200

    added status output

--- build.sh
+++ build.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
 for IMAGE_NAME in $@; do
+    echo "Building ${IMAGE_NAME} ..."
     docker build -t finomnis/${IMAGE_NAME}:latest ${IMAGE_NAME} >${CIRCLE_ARTIFACTS}/${IMAGE_NAME}.out 2>${CIRCLE_ARTIFACTS}/${IMAGE_NAME}.err
 done
