435abb3de51110d851134035447df0ee59dcdff2
drzoidberg <a@b.de>
2015-05-01 13:55:55 +0200

    added output to artifacts

--- build.sh
+++ build.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 for IMAGE_NAME in $1; do
-    docker build -t finomnis/${IMAGE_NAME}:latest ${IMAGE_NAME}
+    docker build -t finomnis/${IMAGE_NAME}:latest ${IMAGE_NAME} >${CIRCLE_ARTIFACTS}/${IMAGE_NAME}.out 2>${CIRCLE_ARTIFACTS}/${IMAGE_NAME}.err
 done
